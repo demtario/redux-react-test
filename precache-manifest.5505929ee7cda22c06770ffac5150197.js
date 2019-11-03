2b0a3af33cbc4d4f5d5b356da6ba3063bf527071
Demtario134 <artik1999@wp.pl>
2019-11-03 17:39:19 +0100

    Updates

---
+++ precache-manifest.5505929ee7cda22c06770ffac5150197.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0630b01ed25568c53db3518d980ac1e",
+    "url": "/rick-and-morty-app/index.html"
+  },
+  {
+    "revision": "ae820e1c2a5689bac659",
+    "url": "/rick-and-morty-app/static/css/main.d3653c64.chunk.css"
+  },
+  {
+    "revision": "39deb612f365ff6541e0",
+    "url": "/rick-and-morty-app/static/js/2.b29a86d4.chunk.js"
+  },
+  {
+    "revision": "ae820e1c2a5689bac659",
+    "url": "/rick-and-morty-app/static/js/main.f5cd1891.chunk.js"
+  },
+  {
+    "revision": "ec3a1f4c776e7fa4d0d7",
+    "url": "/rick-and-morty-app/static/js/runtime-main.21f83d2f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/redux-react-test/precache-manifest.706d1896859edb6ac3675a3477e4e6f5.js"
+  "/rick-and-morty-app/precache-manifest.5505929ee7cda22c06770ffac5150197.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux-react-test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rick-and-morty-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
